e10eccd43df2ddc57a7dc1eef7a850518e993b53
nyangshawbin <shawbin.nyang@circles.asia>
2023-06-17 16:32:47 +0800

    refactor: blanket API error msg for now

--- src/saves/saves.controller.ts
+++ src/saves/saves.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Res, HttpStatus, Post, Body } from '@nestjs/common';
 import { SavesService } from './saves.service';
 import { CreateSaveRequestDto } from './dto/save.dto';
 import { Save } from './schemas/save.schema';
+import { Common } from 'src/library';
 
 @Controller('saves')
 export class SavesController {
@@ -20,9 +21,14 @@ export class SavesController {
     @Body() createSaveDto: CreateSaveRequestDto,
   ) {
     // TODO @shawbin: consider converting dto into entity before passing to service layer
-    const data = await this.saveService.create(createSaveDto);
-    return response
-      .status(HttpStatus.CREATED)
-      .json({ message: 'mock created!', data: data });
+    const { data, error } = await Common.pWrap(
+      this.saveService.create(createSaveDto),
+    );
+
+    if (error) {
+      return response.status(HttpStatus.BAD_REQUEST).json('error');
+    }
+
+    return response.status(HttpStatus.CREATED).json(data);
   }
 }
