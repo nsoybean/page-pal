64d6348cbedc3f07e2e38aa5a89d448eb386fad9
nyangshawbin <shawbin.nyang@circles.asia>
2023-06-27 23:05:52 +0800

    refactor: callback url as env var

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -16,7 +16,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:3000/google/redirect',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       scope: ['email', 'profile'], // set of user information that we require from google needed in our app
     });
   }
