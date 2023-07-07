06544eb666fc4d171b8a7fd611417cd8bca8bb58
nyangshawbin <shawbin.nyang@circles.asia>
2023-07-08 01:28:44 +0800

    refactor: change default port to 3005

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3005;
   await app.listen(port);
 }
 bootstrap();
