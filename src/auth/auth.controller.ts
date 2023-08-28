b12bb189ce49a75829c372f7ac7c6882d322b350
nyangshawbin <shawbin.nyang@circles.asia>
2023-08-28 21:46:35 +0800

    feature: remove unused guard feature: replace logger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,8 @@ import {
   Req,
   Res,
   UseGuards,
+  Logger,
+  Injectable,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
@@ -12,15 +14,19 @@ import { config } from 'dotenv';
 import { AuthService } from './auth.service';
 import { NodeEnv } from './../library/common';
 config();
+
 @Controller('google')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
   constructor(private readonly authService: AuthService) {}
 
   // authentication using 'google' strategy from the passport module
   @Get()
   @UseGuards(AuthGuard('google'))
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async googleAuth(@Req() req) {}
+  async googleAuth(@Req() req, @Res() res: Response) {
+    res.json('okay');
+  }
 
   // auth redirect
   @Get('redirect')
@@ -30,10 +36,10 @@ export class AuthController {
 
     let clientRedirectUrl = '';
     if (process.env.NODE_ENV === NodeEnv.DEVELOPMENT) {
-      console.log('🚀 DEV env:', process.env.LOCAL_CLIENT_URL);
+      this.logger.debug(`env: ${process.env.LOCAL_CLIENT_URL}`);
       clientRedirectUrl = process.env.LOCAL_CLIENT_URL;
     } else {
-      console.log('🚀 DEFAULT env:', process.env.STAGING_CLIENT_URL);
+      this.logger.debug(`env: ${process.env.STAGING_CLIENT_URL}`);
       clientRedirectUrl = process.env.STAGING_CLIENT_URL;
     }
 
